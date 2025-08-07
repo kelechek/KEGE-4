b7115a4aadfca57ee806588f4dd6bdee2d17181b
boris <boris@LAPTOP-655K9220>
2025-08-07 18:58:58 +0200

    Таблица преобразования типов данных

---
+++ PythonBase/lesson-2.py
@@ -0,0 +1,9 @@
+# Таблица преобразования типов данных
+# int -> float(), str(), bool()
+# float -> int(), str(), bool()
+# str -> list(), tuple(), set(), bool()
+# list -> str(), tuple(), set(), bool()
+# tuple -> str(), list(), set(), bool()
+# set -> str(), list(), tuple(), bool()
+# dict -> str(), list(), tuple(), set(), bool()
+# bool -> int(), float(), str()
